f975109a439045f3951f43c14b34340571d54b9c
perkee <hello@perk.ee>
2014-04-06 00:43:50 -0400

    lint and indentation

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,24 @@
 (function($){
 	//onload
 	$(function(){
-	$('a').on('click',function(evt){
-		evt.preventDefault();
-		var jqxhr = $.ajax({
-			url:  this.href,
-			type: 'GET',
-			dataType: 'json'
+		$('a').on('click',function(evt){
+			evt.preventDefault();
+			var jqxhr = $.ajax({
+				url:  this.href,
+				type: 'GET',
+				dataType: 'json'
+			});
+			jqxhr.fail(function(jqxhr,textStatus,errorThrown){
+				console.error(jqxhr,textStatus,errorThrown);
+			});
+			jqxhr.done(function(data){
+				$('#content'      ).html(data.sections.content );
+				$('body>header>h1').html(data.sections.title   );
+				$('head>title'    ).html(data.sections.title   );
+				console.log(null,null,data.path);
+				history.pushState(null,null,data.path);
+			});
+			return false;
 		});
-		jqxhr.fail(function(jqxhr,textStatus,errorThrown){
-			console.error(jqxhr,textStatus,errorThrown);
-		});
-		jqxhr.done(function(data){
-			$('#content'      ).html(data.sections.content );
-			$('body>header>h1').html(data.sections.title   );
-			$('head>title'    ).html(data.sections.title   );
-			console.log(null,null,data.path);
-			history.pushState(null,null,data.path);
-		});
-	});
-		return false;
 	});
 })(jQuery);
